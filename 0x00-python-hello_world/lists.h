803c14d0e9926b30c73d87d8fa8918311977a6ec
Danny Komen <kipropd2@gmail.com>
2022-08-23 12:44:07 -0700

    10

--- 0x00-python-hello_world/lists.h
+++ 0x00-python-hello_world/lists.h
@@ -9,18 +9,17 @@
  * @next: points to the next node
  *
  * Description: singly linked list node structure
- * for Holbertton project
+ * for Holberton project
  */
-
 typedef struct listint_s
 {
 	int n;
 	struct listint_s *next;
 } listint_t;
 
-size_t print_listint(const listint *h);
-listint_t *add_nodeint(listint **head, const int n);
-void free_listint(listint_t *h);
-int check_cycle(listont_t *list);
+size_t print_listint(const listint_t *h);
+listint_t *add_nodeint(listint_t **head, const int n);
+void free_listint(listint_t *head);
+int check_cycle(listint_t *list);
 
 #endif /* LISTS_H */
