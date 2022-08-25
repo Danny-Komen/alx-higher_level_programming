c1899b9e13ef359467ade36da175606ed106cc81
Danny Komen <kipropd2@gmail.com>
2022-08-25 04:04:57 -0700

    0

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-import add_0.add(a, b)
+from add_0 import add(a, b)
 a = 1
 b = 2
-print("{} + {} = {}".format(a, b, add(a, b))
+print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
 
