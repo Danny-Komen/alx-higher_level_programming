bfb4fb07bc47c68baa221351f8e45c15a7fede3c
Danny-Komen <97983723+Danny-Komen@users.noreply.github.com>
2022-08-26 16:09:30 +0300

    2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,10 +2,10 @@
 if __name__ == "__main__":
     from sys import argv
 argc = len(argv)
-if argc < 2:
+if argc < 1:
     print("{} arguments.".format(argc - 1))
 else:
-    if argc == 2:
+    if argc == 1:
         print("{} argument:".format(argc - 1))
     else:
         print("{} arguments:".format(argc - 1))
