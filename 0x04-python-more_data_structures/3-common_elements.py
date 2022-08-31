db17d6c07b820376e39eb90abc0108d8967f6858
Danny-Komen <97983723+Danny-Komen@users.noreply.github.com>
2022-08-31 18:40:27 +0300

    4

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -1,3 +1,9 @@
 #!/usr/bin/python3
 def common_elements(set_1, set_2):
-    return list(set(set_1) & set(set_2))
+    set_1 = list(set_1)
+    set_2 = list(set_2)
+    common_list = []
+    for i in range(len(set_1)):
+        if set_1[i] in set_2:
+            common_list.append(set_1[i])
+    return (common_list)
