caadc76e365cc607bb931e160516ac4abd8e85ff
Danny-Komen <97983723+Danny-Komen@users.noreply.github.com>
2022-09-01 01:19:21 +0300

    1

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    edit_list = my_list [:]
+    edit_list = my_list [ : ]
     for i in range(len(edit_list)):
         if edit_list[i] == search:
             edit_list[i] = replace
