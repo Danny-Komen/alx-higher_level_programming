4a0f45d98facfed7dbaf544114aedab0c5db6b16
Danny-Komen <97983723+Danny-Komen@users.noreply.github.com>
2022-09-15 00:45:38 +0300

    1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Define class Square."""
+
+
+class Square:
+    """Represents a square."""
+
+    def __init__(self, new_size):
+        """initialize a new square.
+        Args:
+            size (int): The size of the new square.
+        """
+        self.__size = new_size
