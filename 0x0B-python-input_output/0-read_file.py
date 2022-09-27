36ac2ca34e28cd916f273f8fe41f2ce2667c3ba4
Danny-Komen <97983723+Danny-Komen@users.noreply.github.com>
2022-09-27 21:10:37 +0300

    0

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,4 +1,13 @@
 #!/usr/bin/python3
+"""
+function that reads a text file
+"""
+
+
 def read_file(filename=""):
+    """
+    Reads a text file
+    Returns none
+    """
     with open(filename, "r", encoding="uft-8") as f:
         print(f.read(), end="")
