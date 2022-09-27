6057dbe129f046b87386a0ae362e99a63a303609
Danny-Komen <97983723+Danny-Komen@users.noreply.github.com>
2022-09-27 22:24:52 +0300

    6

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -12,4 +12,4 @@ def load_from_json_file(filename):
     Returns a python object
     """
     with open(filename, "r", encoding="utf=8") as f:
-        return json.loads(f)
+        return json.load(f)
