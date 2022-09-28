697e05bbf3d65dfa7a1b04659c8791e912db0e40
Danny-Komen <97983723+Danny-Komen@users.noreply.github.com>
2022-09-28 21:00:52 +0300

    8

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+class dictionary
+"""
+
+
+def class_to_json(obj):
+    """
+    the function defines class dictionary
+    Returns a dictionary
+    """
+    return obj.__dict__
